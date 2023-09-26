d82bbdb3bb82a4a5767c87de6692286c0627406a
Camilo Almeida <camiloalmeida22@gmail.com>
2023-09-26 19:20:23 -0300

    poo

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+g
+7src/edu/camilo/java/poo/construtor/SistemaCadastro.java,e\6\e6c152d2cdd510a662ec7e91b9e9561037c8ab70
+^
+.src/edu/camilo/java/poo/construtor/Pessoa.java,1\6\1639a6571a2257ad3c171c55384c9da04b137a14
---
+++ src/edu/camilo/java/poo/collections/list/operacoesBasicas/ListaTarefas.java
@@ -0,0 +1,44 @@
+package edu.camilo.java.poo.collections.list.operacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t: tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+        listaTarefas.adicionarTarefa("tarefa 1");
+        listaTarefas.adicionarTarefa("tarefa 1");
+        listaTarefas.adicionarTarefa("tarefa 2");
+        listaTarefas.removerTarefa("tarefa 2");
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.obterDescricoesTarefas();
+    }
+}
+
+
